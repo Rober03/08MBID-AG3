f54d69a14be2e6d48b285fb8457f7a3f7e4097d3
Rober03 <robertojbote@gmail.com>
2023-04-29 19:39:19 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 // Import stylesheets
+
 import './style.css';
 
-// Write Javascript code!
 const appDiv = document.getElementById('app');
+
+d3.csv(
+  'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv',
+  function (error, data) {
+    var visualization = d3plus
+      .viz()
+
+      .container('#viz1')
+
+      .data([
+        { year: 1991, name: 'alpha', value: 15 },
+        { year: 1992, name: 'alpha', value: 20 },
+      ])
+
+      .type('bar')
+
+      .id('name')
+
+      .x('year')
+
+      .y('value')
+
+      .draw();
+  }
+);
+
+d3.json(
+  'https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json',
+  function (data) {
+    var visualization = d3plus
+      .viz()
+
+      .container('#viz2')
+
+      .data(data)
+
+      .type('bar')
+
+      .id('name')
+
+      .x('year')
+
+      .y('value')
+
+      .axes({ ticks: 'false' })
+
+      .draw();
+  }
+);
